6786b21581d6d7317f7c11e89be54f3705bd90c5
Nicolas-jchr <Nicolas.jchr@gmail.com>
2024-05-22 12:52:20 +0200

    Create modals for projects and JS done for open/close modals

---
+++ script.js
@@ -0,0 +1,24 @@
+function openModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.style.display = "block";
+    setTimeout(function() {
+        modal.classList.add('show');
+    }, 10);
+}
+
+function closeModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.classList.remove('show');
+    setTimeout(function() {
+        modal.style.display = "none";
+    }, 300);
+}
+
+window.onclick = function(event) {
+    const modals = document.getElementsByClassName('modal');
+    for (const i = 0; i < modals.length; i++) {
+        if (event.target == modals[i]) {
+            closeModal(modals[i].id);
+        }
+    }
+}
