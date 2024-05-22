a9ec5c764f1cc81fd785bf9c4704ba6c10d8fa67
Nicolas-jchr <Nicolas.jchr@gmail.com>
2024-05-22 21:15:14 +0200

    fix some css in all page

--- script.js
+++ script.js
@@ -2,22 +2,22 @@ function openModal(modalId) {
     const modal = document.getElementById(modalId);
     modal.style.display = "block";
     setTimeout(function() {
-        modal.classList.add('show');
+        modal.classList.add("show");
     }, 10);
 }
 
 function closeModal(modalId) {
     const modal = document.getElementById(modalId);
-    modal.classList.remove('show');
+    modal.classList.remove("show");
     setTimeout(function() {
         modal.style.display = "none";
     }, 300);
 }
 
 function closeAllModals() {
-    const modals = document.getElementsByClassName('modal');
+    const modals = document.getElementsByClassName("modal");
     for (let i = 0; i < modals.length; i++) {
-        modals[i].classList.remove('show');
+        modals[i].classList.remove("show");
         setTimeout(function() {
             modals[i].style.display = "none";
         }, 300);
@@ -25,11 +25,10 @@ function closeAllModals() {
 }
 
 window.onclick = function(event) {
-    const modals = document.getElementsByClassName('modal');
+    const modals = document.getElementsByClassName("modal");
     for (let i = 0; i < modals.length; i++) {
         if (event.target == modals[i]) {
             closeAllModals();
-            break;
         }
     }
 }
